fed50c8858bdcc3c2a9bcdaeda3626c34420e27d
faizanshahid232 <75093651+faizanshahid232@users.noreply.github.com>
2023-07-18 23:00:19 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,113 @@
+const express = require('express');
+var cors = require('cors')
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+
+app.use(express.static("public"));
+app.use(express.json());
+
+const stripe = require("stripe")('sk_test_51NOz1fIi4beyPjru1XAPSAdxY1x8zH8fJMOghajQGbgq2SVgE3R2tLTj8fhoZ8kCJHq7wX0PKgstks4S6NUBwRYA006SNgD679');
+
+
+var whitelist = [
+    'http://localhost:3000',
+];
+var corsOptions = {
+    origin: function(origin, callback){
+        var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+        callback(null, originIsWhitelisted);
+    },
+    credentials: true
+};
+app.use(cors(corsOptions));
+
+app.get('/', (req, res) => {
+    res.send('Welcome to CORS server 😁')
+})
+
+const calculateOrderAmount = (items) => {
+    // Replace this constant with a calculation of the order's amount
+    // Calculate the order total on the server to prevent
+    // people from directly manipulating the amount on the client
+    return items;
+  };
+  
+  app.post("/create-payment-intent", async (req, res) => {
+    res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+    const { items } = req.body;
+      console.log("here1", items);
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: calculateOrderAmount(items),
+      currency: "usd",
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
+    console.log("Payment:", paymentIntent.client_secret);
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+      
+    });
+  });
+  
+app.post('/cors-post', (req, res) => {
+    res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+    const url = 'https://carboncompensate.climatepositive.com/api/v1/calculate';
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Authorization": "CPF5A58B2108704667ACABF945230F0D61:WQ3ibsd3EQfNuvQmoLR5ygrd8mCUVfSuk47fufXs8Jnziu5fIeXFDXVFPPhJRiq2",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req.body.json),
+    })
+    .then((response) => response.json())
+    .then(data => {
+        console.log(data);
+        res.send(data)
+    })
+    
+})
+
+app.post('/cors-post-email', (req, res) => {
+    res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+    console.log(req.body);
+    const url = 'https://carboncompensate.climatepositive.com/api/v1/vend';
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Authorization": "CPF5A58B2108704667ACABF945230F0D61:WQ3ibsd3EQfNuvQmoLR5ygrd8mCUVfSuk47fufXs8Jnziu5fIeXFDXVFPPhJRiq2",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req.body.mergedObj),
+    })
+    .then((response) => response.json())
+    .then(data => {
+        console.log(data);
+        res.send(data)
+    })
+    
+})
+app.get('/cors', (req, res) => {
+    res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+    
+    const url = 'https://carboncompensate.starcb.com/api/v1';
+    fetch(url, {
+        headers: {
+            "Authorization": "CPF5A58B2108704667ACABF945230F0D61:WQ3ibsd3EQfNuvQmoLR5ygrd8mCUVfSuk47fufXs8Jnziu5fIeXFDXVFPPhJRiq2",
+        },
+    })
+    .then((response) => response.json())
+    .then(data => {
+        console.log(data);
+        res.send('This has CORS enabled 🎈'+ data)
+    })
+    
+})
+app.listen(8080, () => {
+    console.log('listening on port 8080')
+})
