a9972e151ab59a99b776c340d540de80752730b7
faizanshahid232 <75093651+faizanshahid232@users.noreply.github.com>
2023-07-18 23:22:16 +0400

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,5 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
-  })
-
-app.listen(process.env.PORT || 3000);
+  });
 module.exports = app;
