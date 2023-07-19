b4f714d8d655e6337800d1806026ae1afef1d168
faizanshahid232 <75093651+faizanshahid232@users.noreply.github.com>
2023-07-20 01:11:13 +0400

    first commit

--- api/index.js
+++ api/index.js
@@ -14,8 +14,8 @@ const allowCors = fn => async (req, res) => {
     res.setHeader('Access-Control-Allow-Credentials', true)
     res.setHeader('Access-Control-Allow-Origin', '*')
     // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+     res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    //res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
     res.setHeader(
       'Access-Control-Allow-Headers',
       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
@@ -32,7 +32,7 @@ const allowCors = fn => async (req, res) => {
     res.end(d.toString())
   }
   
-  
+  module.exports = allowCors(handler);  
   
 module.exports = [
     'strapi::errors',
@@ -162,5 +162,5 @@ app.get('/cors', (req, res) => {
     })
     
 })
-module.exports = allowCors(handler);
+
 module.exports = app;
